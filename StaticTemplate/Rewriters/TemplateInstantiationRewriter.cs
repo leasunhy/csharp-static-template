909b1c9cc5696bffaa8f1f54b472ce8dd051fca7
leasunhy <leasunhy@gmail.com>
2017-03-28 21:04:20 +0800

    add documents for TemplateInstantiationRewriter.cs

--- StaticTemplate/Rewriters/TemplateInstantiationRewriter.cs
+++ StaticTemplate/Rewriters/TemplateInstantiationRewriter.cs
@@ -12,24 +12,36 @@ using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace StaticTemplate.Rewriters
 {
+    /// <summary>
+    /// Rewrites a template into a template instance for given type arguments.
+    /// </summary>
     public class TemplateInstantiationRewriter : CSharpSyntaxRewriter
     {
         private readonly Dictionary<string, INamedTypeSymbol> _typeMap;
         private readonly SyntaxToken _instName;
         private bool _inClassDeclaration = false;
 
-        private TemplateInstantiationRewriter(ClassDeclarationSyntax template, string instName, IEnumerable<INamedTypeSymbol> typeArgs)
+        private TemplateInstantiationRewriter(ClassDeclarationSyntax template, string instName,
+                                              IEnumerable<INamedTypeSymbol> typeArgs)
         {
             var typeArgLst = typeArgs.ToList();
             if (template.TypeParameterList.Parameters.Count != typeArgLst.Count)
                 throw new InvalidOperationException("Type arguments should be as many as type parameters");
             _typeMap = template.TypeParameterList
-                              .Parameters
-                              .Zip(typeArgLst, (p, a) => Tuple.Create(p.ToString(), a))
-                              .ToDictionary(_ => _.Item1, _ => _.Item2);
+                               .Parameters
+                               .Zip(typeArgLst, (p, a) => Tuple.Create(p.ToString(), a))
+                               .ToDictionary(_ => _.Item1, _ => _.Item2);
             _instName = IdentifierName(instName).Identifier;
         }
 
+        // TODO(leasunhy): enable nested static templates.
+        /// <summary>
+        /// Visits a <see cref="ClassDeclarationSyntax"/> which must be the template
+        /// if working on a template isolation.
+        /// <para>This method removes type parameter list of the template as well.</para>
+        /// </summary>
+        /// <param name="node">The syntax node to visit.</param>
+        /// <returns></returns>
         public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
         {
             _inClassDeclaration = true;
@@ -39,8 +51,17 @@ namespace StaticTemplate.Rewriters
             return orig.RemoveNode(typeparams, SyntaxRemoveOptions.KeepExteriorTrivia).WithIdentifier(_instName);
         }
 
+        /// <summary>
+        /// Rewrites the <see cref="IdentifierNameSyntax"/> with the type parameters of the
+        /// template substited with the type arguments.
+        /// </summary>
+        /// <param name="node">The syntax node to visit.</param>
+        /// <returns>The syntax node as described above.</returns>
         public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node)
         {
+            // We only handle the IdentifierNameSyntax nodes inside a ClassDeclarationSyntax.
+            // Given we are working on a template isolation, the ClassDeclarationSyntax must
+            // be the template itself.
             if (!_inClassDeclaration) return node;
 
             // note that we don't need to check whether node is a TypeSyntax,
@@ -55,6 +76,16 @@ namespace StaticTemplate.Rewriters
             return node;
         }
 
+        /// <summary>
+        /// Rewrites a template into a template instance for a given list of arguments.
+        /// </summary>
+        /// <param name="compilationUnit">
+        /// The template isolation as described in <see cref="TemplateIsolationRewriter"/>.
+        /// </param>
+        /// <param name="template">The template to rewrite.</param>
+        /// <param name="instName">The name of the template instance.</param>
+        /// <param name="typeArgs">The list of type arguments for the instantiation.</param>
+        /// <returns></returns>
         public static SyntaxTree InstantiateFor(CompilationUnitSyntax compilationUnit,
                                                 ClassDeclarationSyntax template,
                                                 string instName,
