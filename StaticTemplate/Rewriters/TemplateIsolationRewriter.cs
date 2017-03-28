eca502bfad24c77e10190091cb3b816ecbef399d
leasunhy <leasunhy@gmail.com>
2017-03-28 21:04:20 +0800

    add documents for TemplateIsolationRewriter.cs

--- StaticTemplate/Rewriters/TemplateIsolationRewriter.cs
+++ StaticTemplate/Rewriters/TemplateIsolationRewriter.cs
@@ -9,13 +9,47 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace StaticTemplate.Rewriters
 {
+    /// <summary>
+    /// Isolates a template from a <see cref="CompilationUnitSyntax"/>.
+    /// The isolation can be used for instantiating the template.
+    /// </summary>
+    /// <example>
+    /// Given target class template TemplateTarget and code:
+    /// <code>
+    /// using System;
+    /// namespace Example
+    /// {
+    ///     using System.Linq;
+    ///
+    ///     [StaticTemplate]
+    ///     class TemplateTarget&lt;T&gt;
+    ///
+    ///     [StaticTemplate]
+    ///     class TemplateNonTarget&lt;T&gt;
+    ///
+    ///     class NonTemplate {}
+    /// }
+    /// </code>
+    /// The isolation results in code:
+    /// <code>
+    /// using System;
+    /// namespace Example
+    /// {
+    ///     using System.Linq;
+    ///
+    ///     [StaticTemplate]
+    ///     class TemplateTarget&lt;T&gt;
+    /// }
+    /// </code>
+    /// And the isolation is thus useful for instantiations.
+    /// </example>
     class TemplateIsolationRewriter : CSharpSyntaxRewriter
     {
         private readonly ClassDeclarationSyntax _target;
 
         private TemplateIsolationRewriter(ClassDeclarationSyntax target)
         {
-            this._target = target;
+            _target = target;
         }
 
         public override SyntaxNode VisitNamespaceDeclaration(NamespaceDeclarationSyntax node)
@@ -24,6 +58,11 @@ namespace StaticTemplate.Rewriters
             // TODO(leasunhy): extra processing here: what other types of nodes shall be removed?
         }
 
+        /// <summary>
+        /// Visits a <see cref="ClassDeclarationSyntax"/>. If it is not the target, remove it.
+        /// </summary>
+        /// <param name="node">The node to visit.</param>
+        /// <returns>Null if it is not the target template to isolate; a cleaned version of the template if it is.</returns>
         public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
         {
             if (node != _target)
@@ -31,6 +70,11 @@ namespace StaticTemplate.Rewriters
             return ClassTemplate.CleanClassTemplate(node);
         }
 
+        /// <summary>
+        /// Given a template target that belongs to a <see cref="CompilationUnitSyntax"/>, isolate it.
+        /// </summary>
+        /// <param name="target">The template to isolate.</param>
+        /// <returns>A new <see cref="CompilationUnitSyntax"/> with the template isolated.</returns>
         public static CompilationUnitSyntax IsolateFor(ClassDeclarationSyntax target)
         {
             return (CompilationUnitSyntax)new TemplateIsolationRewriter(target).Visit(target.SyntaxTree.GetRoot());
